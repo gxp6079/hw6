1cc8599468e9c769ff935a8f32b80b763ea7ea10
gihpuccini <gxp6079@g.rit.edu>
2018-10-25 14:55:56 -0400

    Fixed memory allocation error and freeing

--- main.c
+++ main.c
@@ -1,3 +1,10 @@
+
+/*
+ * file: HeapADT.c
+ * @author: Giovana Puccini
+ * @email: gxp6079@rit.edu
+ * --------------------------------------------
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
@@ -24,21 +31,20 @@ int right_child(int i){
 }
 
 Heap createHeap(size_t capacity
-        , int (*compFun)(const void * lhs, const void * rhs)
-        , void (*dumpEntry) (const void * item, FILE * outfp)){
+                , int (*compFun)(const void * lhs, const void * rhs)
+                , void (*dumpEntry) (const void * item, FILE * outfp)){
     Heap new_heap;
     new_heap = (Heap) malloc(sizeof(struct Heap_S));
     new_heap->size = 0;
     new_heap->capacity = capacity;
     new_heap->dumpEntry = dumpEntry;
     new_heap->compFu = compFun;
+    new_heap->array = (void*) malloc(capacity * sizeof(void*));
     return new_heap;
 }
 
 void destroyHeap( Heap aHeap ){
-    for(int i = aHeap->size - 1 ; i >= 0 ; i--){
-        free(aHeap->array[i]);
-    }
+    free(aHeap->array);
     free(aHeap);
 }
 
@@ -51,23 +57,24 @@ const void * topHeap(const Heap aHeap){
 }
 
 void * removeTopHeap(Heap aHeap){
-    void * removed = aHeap->array[0];
+    void * removed = (void *) aHeap->array[0];
     int last_idx = aHeap->size -1;
     aHeap->array[0] = aHeap->array[last_idx];
+    aHeap->size --;
     int idx = 0;
     int idx_left = left_child(0);
     int idx_right = right_child(0);
-    while((idx_left < aHeap->size && idx_right < aHeap->size) && ((aHeap->compFu(aHeap->array[idx], aHeap->array[idx_left]) == 0) ||
-            (aHeap->compFu(aHeap->array[idx], aHeap->array[idx_right]) == 0))){
+    while((idx_left < (int) aHeap->size && idx_right < (int) aHeap->size) && ((aHeap->compFu(aHeap->array[idx], aHeap->array[idx_left]) == 0) ||
+                                                                              (aHeap->compFu(aHeap->array[idx], aHeap->array[idx_right]) == 0))){
         if(aHeap->compFu(aHeap->array[idx_right], aHeap->array[idx_left]) == 0){
-            void * temp = aHeap->array[idx];
+            const void * temp = aHeap->array[idx];
             aHeap->array[idx] = aHeap->array[idx_left];
             aHeap->array[idx_left] = temp;;
             idx = idx_left;
             idx_left = left_child(idx);
             idx_right = right_child(idx);
         } else{
-            void * temp = aHeap->array[idx];
+            const void * temp = aHeap->array[idx];
             aHeap->array[idx] = aHeap->array[idx_right];
             aHeap->array[idx_right] = temp;
             idx = idx_right;
@@ -75,30 +82,25 @@ void * removeTopHeap(Heap aHeap){
             idx_right = right_child(idx);
         }
     }
-    aHeap->size --;
     return removed;
 }
 
 void insertHeapItem(Heap aHeap, const void * item) {
     int idx = aHeap->size;
-    if (idx == 0) {
-        aHeap->array = malloc(sizeof(item));
-    } else {
-        if (idx == aHeap->capacity) {
-            aHeap->capacity = 2 * aHeap->capacity;
-        }
-        aHeap->array = realloc(aHeap->array, sizeof(int));
+    if (idx == aHeap->capacity) {
+        aHeap->capacity = 2 * aHeap->capacity;
+        aHeap->array = realloc(aHeap->array, aHeap->capacity * sizeof(void*));
     }
-    aHeap->size++;
     aHeap->array[idx] = item;
     int parent_indx = parent(idx);
     while (idx != 0 && aHeap->compFu(aHeap->array[parent_indx], aHeap->array[idx]) == 0) {
-        void * temp = aHeap->array[idx];
+        const void * temp = aHeap->array[idx];
         aHeap->array[idx] = aHeap->array[parent_indx];
         aHeap->array[parent_indx] = temp;
         idx = parent_indx;
         parent_indx = parent(parent_indx);
     }
+    aHeap->size++;
 }
 
 
@@ -107,4 +109,3 @@ void dumpHeap(Heap aHeap, FILE * outfp){
         aHeap->dumpEntry(aHeap->array[i], outfp);
     }
 }
-
